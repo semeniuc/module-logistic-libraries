523b625bb14ec4456426d6e7ebe6fd022ccc9116
Vadim Semeniuc <semeniuc@int.pl>
2024-07-17 16:05:55 +0300

    fix: connect files

--- views/components/header.php
+++ views/components/header.php
@@ -1,5 +1,7 @@
 <?php
 
+$publicDirectory = dirname($_SERVER['SCRIPT_NAME']);
+
 ?>
 
 <!DOCTYPE html>
@@ -7,7 +9,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?></title>
-    <link href="/assets/css/app.css" rel="stylesheet">
+    <link href="<?= $publicDirectory ?>/assets/css/app.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
 </head>
