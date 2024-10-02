ed550042e5efe89fe053e291a25435a404e0d5cf
semeniuc <pussword@gmail.com>
2024-10-02 19:04:29 +0300

    fix: prepare for deploy

--- config/tariff-library/auto.php
+++ config/tariff-library/auto.php
@@ -8,35 +8,35 @@ return [
         'station' => [
             'id' => [
                 'dev' => 'UF_CRM_6_STATION',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_STATION',
             ],
             'column' => 'A'
         ],
         'point' => [
             'id' => [
                 'dev' => 'UF_CRM_6_POINT',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_POINT',
             ],
             'column' => 'B'
         ],
         'contractor' => [
             'id' => [
                 'dev' => 'UF_CRM_6_CONTRACTOR',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_CONTRACTOR',
             ],
             'column' => 'C'
         ],
         'deliveryCost20DryLess18' => [
             'id' => [
                 'dev' => 'UF_CRM_6_DELIVERY_COST_20DRY_LESS_18',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_DELIVERY_COST_20DRY_LESS_18',
             ],
             'column' => 'D'
         ],
         'deliveryCost40HcLess20' => [
             'id' => [
                 'dev' => 'UF_CRM_6_DELIVERY_COST_40HC_LESS_20',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_DELIVERY_COST_40HC_LESS_20',
             ],
             'column' => 'E'
         ],
@@ -44,56 +44,56 @@ return [
         'deliveryPriceValidFrom' => [
             'id' => [
                 'dev' => 'UF_CRM_6_DELIVERY_PRICE_VALID_FROM',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_DELIVERY_PRICE_VALID_FROM',
             ],
             'column' => 'F'
         ],
         'deliveryPriceValidTill' => [
             'id' => [
                 'dev' => 'UF_CRM_6_DELIVERY_PRICE_VALID_TILL',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_DELIVERY_PRICE_VALID_TILL',
             ],
             'column' => 'G'
         ],
         'terminal20Dry' => [
             'id' => [
                 'dev' => 'UF_CRM_6_TERMINAL_20DRY',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_TERMINAL_20DRY',
             ],
             'column' => 'H'
         ],
         'terminal40Hc' => [
             'id' => [
                 'dev' => 'UF_CRM_6_TERMINAL_40HC',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_TERMINAL_40HC',
             ],
             'column' => 'I'
         ],
         'terminalValidFrom' => [
             'id' => [
                 'dev' => 'UF_CRM_6_TERMINAL_VALID_FROM',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_TERMINAL_VALID_FROM',
             ],
             'column' => 'J'
         ],
         'terminalValidTill' => [
             'id' => [
                 'dev' => 'UF_CRM_6_TERMINAL_VALID_TILL',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_TERMINAL_VALID_TILL',
             ],
             'column' => 'K'
         ],
         'deliveryComment' => [
             'id' => [
                 'dev' => 'UF_CRM_6_DELIVERY_COMMENT',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_DELIVERY_COMMENT',
             ],
             'column' => 'L'
         ],
         'terminalComment' => [
             'id' => [
                 'dev' => 'UF_CRM_6_TERMINAL_COMMENT',
-                'prod' => null,
+                'prod' => 'UF_CRM_6_TERMINAL_COMMENT',
             ],
             'column' => 'M'
         ],
--- config/tariff-library/railway.php
+++ config/tariff-library/railway.php
@@ -8,105 +8,105 @@ return [
         'pod' => [
             'id' => [
                 'dev' => 'UF_CRM_5_POD',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_POD',
             ],
             'column' => 'A'
         ],
         'point' => [
             'id' => [
                 'dev' => 'UF_CRM_5_POINT',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_POINT',
             ],
             'column' => 'B'
         ],
         'station' => [
             'id' => [
                 'dev' => 'UF_CRM_5_STATION',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_STATION',
             ],
             'column' => 'C'
         ],
         'contractor' => [
             'id' => [
                 'dev' => 'UF_CRM_5_CONTRACTOR',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_CONTRACTOR',
             ],
             'column' => 'D'
         ],
         'deliveryCostCoc20DryLess24' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_COST_COC_20DRY_LESS_24',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_COST_COC_20DRY_LESS_24',
             ],
             'column' => 'E'
         ],
         'deliveryCostCoc20DryMore24' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_COST_COC_20DRY_MORE_24',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_COST_COC_20DRY_MORE_24',
             ],
             'column' => 'F'
         ],
         'deliveryCostCoc40Hc' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_COST_COC_40HC',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_COST_COC_40HC',
             ],
             'column' => 'G'
         ],
         'deliveryCostSoc20DryLess24' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_COST_SOC_20DRY_LESS_24',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_COST_SOC_20DRY_LESS_24',
             ],
             'column' => 'H'
         ],
         'deliveryCostSoc20DryMore24' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_COST_SOC_20DRY_MORE_24',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_COST_SOC_20DRY_MORE_24',
             ],
             'column' => 'I'
         ],
         'deliveryCostSoc40Hc' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_COST_SOC_40HC',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_COST_SOC_40HC',
             ],
             'column' => 'J'
         ],
         'securityCost20Dry' => [
             'id' => [
                 'dev' => 'UF_CRM_5_SECURITY_COST_20DRY',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_SECURITY_COST_20DRY',
             ],
             'column' => 'K'
         ],
         'securityCost40Hc' => [
             'id' => [
                 'dev' => 'UF_CRM_5_SECURITY_COST_40HC',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_SECURITY_COST_40HC',
             ],
             'column' => 'L'
         ],
         'deliveryPriceValidFrom' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_PRICE_VALID_FROM',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_PRICE_VALID_FROM',
             ],
             'column' => 'M'
         ],
         'deliveryPriceValidTill' => [
             'id' => [
                 'dev' => 'UF_CRM_5_DELIVERY_PRICE_VALID_TILL',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_DELIVERY_PRICE_VALID_TILL',
             ],
             'column' => 'N'
         ],
         'comment' => [
             'id' => [
                 'dev' => 'UF_CRM_5_COMMENT',
-                'prod' => null,
+                'prod' => 'UF_CRM_5_COMMENT',
             ],
             'column' => 'O'
         ],
